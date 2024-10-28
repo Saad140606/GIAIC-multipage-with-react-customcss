9fc45bec66601fcea7ad613f7ac08fa68cc46c86
Saad140606 <syedsaadnajam2006@gmail.com>
2024-10-28 11:15:14 +0500

    first commit

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+import { footerContact, footerItem, socialIcons } from "../data/Data";
+import Newsletter from "../home/Newsletter";
+
+export default function Footer() {
+  return (
+    <>
+      <Newsletter />
+      <div
+        className="container-fluid bg-dark text-light footer wow fadeIn"
+        data-wow-delay="0.1s"
+      >
+        <div className="container pb-5">
+          <div className="row g-5">
+            <div className="col-md-6 col-lg-4">
+              <div className="bg-primary rounded p-4">
+                <Link to="/">
+                  <h1 className="text-white text-uppercase mb-3">StayEase</h1>
+                </Link>
+                <p className="text-white mb-0">
+                StayEase offers a blend of comfort and elegance, providing a peaceful retreat in the heart of the city. 
+                Our modern amenities, warm hospitality, and cozy ambiance ensure a delightful stay for every guest. 
+                Discover relaxation and convenience with us at StayEase.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3">
+              <h6 className="section-title text-start text-primary text-uppercase mb-4">
+                Contact
+              </h6>
+              {footerContact.map((val, index) => (
+                <p className="mb-2" key={index}>
+                  {val.icon} {val.name}
+                </p>
+              ))}
+              <div className="d-flex pt-2">
+                {socialIcons.slice(0, 4).map((val, index) => (
+                  <a className="btn btn-outline-light btn-social" href="">
+                    {val.icon}
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div className="col-lg-5 col-md-12">
+              <div className="row gy-5 g-4">
+                {footerItem.map((section, sectionIndex) => (
+                  <div className="col-md-6" key={sectionIndex}>
+                    <h6 className="section-title text-start text-primary text-uppercase mb-4">
+                      {section.header}
+                    </h6>
+                    {section.UnitItem.map((item, itemIndex) => (
+                      <a className="btn btn-link" href="" key={itemIndex}>
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
